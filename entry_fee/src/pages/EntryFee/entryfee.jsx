eda088f33e80365c1a5dac175fb0127a85e3114d
shilpa_v <55580071+ShilpaVinayaka@users.noreply.github.com>
2021-10-27 21:36:32 +0530

    Update entryfee.jsx

--- entry_fee/src/pages/EntryFee/entryfee.jsx
+++ entry_fee/src/pages/EntryFee/entryfee.jsx
@@ -1,28 +1,43 @@
 import "./entryfee.css";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-export default function EntryFee() {
+export default function EntryFee(props) {
   const [radioData, setRadioData] = useState({
-    radio: ""
-  })
-  const handleChange = event => {
+    radio: "",
+  });
+  const handleChange = (event) => {
     var x = document.getElementsByClassName("PaidEntry")[0];
     const target = event.target;
     var name = target.name;
     var value = target.value;
     setRadioData({
       ...radioData,
-      [name] : value
-    })
+      [name]: value,
+    });
 
-    if (value == 'free')
-      x.style.display = "none";
-    else 
-      x.style.display = "flex";
+    if (value == "free") x.style.display = "none";
+    else x.style.display = "flex";
+  };
 
+  const [amount, setAmount] = useState(1);
+  const [amountInfromCC, setAmountInFromCC] = useState(true);
 
+  let toAmount, fromAmount;
+  if (amountInfromCC) {
+    fromAmount = amount;
+    toAmount = amount / 10;
+  } else {
+    toAmount = amount;
+    fromAmount = amount * 10;
   }
-
+  function handleFromAmountChange(e) {
+    setAmount(e.target.value);
+    setAmountInFromCC(true);
+  }
+  function handleToAmountChange(e) {
+    setAmount(e.target.value);
+    setAmountInFromCC(false);
+  }
   return (
     <div className="entryfeeContainer">
       <div className="entryfee">
@@ -38,12 +53,23 @@ export default function EntryFee() {
         <div className="options">
           <label className="entryfeeOptions">
             <span>Free</span>
-            <input type="radio" name="radio" value="free" onChange={handleChange} checked={radioData.radio == "free"} />
-
+            <input
+              type="radio"
+              name="radio"
+              value="free"
+              onChange={handleChange}
+              checked={radioData.radio == "free"}
+            />
             <span className="entryFeeCheck"></span>
           </label>
           <label className="entryfeeOptions">
-            <input type="radio" name="radio"  value="paid" onChange={handleChange} checked={radioData.radio == "paid"}/>
+            <input
+              type="radio"
+              name="radio"
+              value="paid"
+              onChange={handleChange}
+              checked={radioData.radio == "paid"}
+            />
             <span>Paid</span>
             <span className="entryFeeCheck"></span>
           </label>
@@ -52,7 +78,14 @@ export default function EntryFee() {
         <div className="PaidEntry">
           <div className="enterFeeInput">
             <span className="coin">
-              <input type="number" name="coins" className="coinCount" placeholder="Entry Fee in CC"/>
+              <input
+                type="number"
+                name="coins"
+                className="coinCount"
+                placeholder="Entry Fee in CC"
+                value={fromAmount}
+                onChange={handleFromAmountChange}
+              />
               <span> coins</span>
             </span>
             <span className="coin">
@@ -61,7 +94,10 @@ export default function EntryFee() {
                 name="inr"
                 className="coinCount"
                 placeholder="In INR"
+                value={toAmount}
+                onChange={handleToAmountChange}
               />
+              <span> INR</span>
             </span>
           </div>
         </div>
